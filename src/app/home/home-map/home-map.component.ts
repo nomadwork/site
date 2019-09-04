0f15485059007ece36c63b00449f661a3c74a07a
Aluno <aluno@expert.local>
2019-09-04 19:38:43 -0300

    mock markers + mostrando markers no mapa +popup nos markers

--- src/app/home/home-map/home-map.component.ts
+++ src/app/home/home-map/home-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-
+import { LoginService } from '../../services/login.service'
 
 @Component({
   selector: 'app-home-map',
@@ -26,25 +26,36 @@ export class HomeMapComponent implements OnInit {
   };
 
   onMapReady(map: L.Map) {
-    var myIcon = L.icon({
-      // iconSize:[32,48],
+    const myIcon = L.icon({
+      popupAnchor:[7,-35],
       iconAnchor: [25, 50],
       iconUrl: 'src/../../../assets/img/my-pin.png'
-    })
+    });
+
     if (navigator) {
       navigator.geolocation.getCurrentPosition(pos => {
         const { latitude, longitude } = pos.coords;
 
         map.panTo([latitude, longitude])
-        L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
+
+        this.loginService.markers().subscribe((data)=>{
+          data.marker.forEach(marker =>{
+
+         const nws =  L.marker([marker.latitude, marker.longitude]).addTo(map);
+         nws.bindPopup(marker.name);
+          })
+        })
+        const currentPosition = L.marker([latitude, longitude],{ icon: myIcon }).addTo(map);
+        currentPosition.bindPopup("Você está aqui").openPopup();
 
       }, err => console.error(err), this.option);
     }
   }
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
 
-  ngOnInit() { }
+  ngOnInit() { 
+  }
 
 }
