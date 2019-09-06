2b47b0f46a74453c5352bbb0802b885d137c595f
pdroalbqrq <pdroalbqrq@gmail.com>
2019-09-05 21:55:10 -0300

    botao para centralizar o mapa no usuario

--- src/app/home/home-map/home-map.component.ts
+++ src/app/home/home-map/home-map.component.ts
@@ -9,25 +9,32 @@ import { LoginService } from '../../services/login.service'
 })
 export class HomeMapComponent implements OnInit {
 
-  iconUrl: string = 'src/../../../assets/img/my-pin.svg'
+  iconUrl: string = 'src/../../../assets/img/my-pin.svg';
+  map;
+  geoLocation;
 
-  option: any = {
+  config: any = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
   };
 
+
   options = {
     layers: [
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>' })
     ],
-    zoom: 15,
-    center: L.latLng(-8.0631, -34.8713)
+    // zoom: 15,
+    center: []
   };
 
   onMapReady(map: L.Map) {
-    
-    const myIcon = L.icon({
+
+    this.map = map;
+
+
+    // CONFIGURAÇÃO DOS MARCADORES
+    const userIcon = L.icon({
       popupAnchor: [7, -35],
       iconAnchor: [25, 50],
       iconUrl: 'src/../../../assets/img/my-pin.png'
@@ -35,40 +42,62 @@ export class HomeMapComponent implements OnInit {
     const nwsIcon = L.icon({
       popupAnchor: [-3, -35],
       iconAnchor: [36, 45],
-      className:'blink',
+      className: 'blink',
       iconUrl: 'src/../../../assets/img/nws-pin.png'
     });
+
+    // CONFIGURAÇÃO DOS POPUPS
     const customNwsPopup = {
+      className: 'nws',
+      maxWidth: 500,
+      closeButton: false
+    }
+    const customUserPopup = {
       className: 'nws',
       maxWidth: 500,
       closeButton: false
     }
 
 
+
     if (navigator) {
-      navigator.geolocation.getCurrentPosition(pos => {
+      navigator.geolocation.getCurrentPosition(async pos => {
+        this.geoLocation = pos;
         const { latitude, longitude } = pos.coords;
+        map.setView([latitude, longitude],15)
 
-        map.panTo([latitude, longitude])
+        const currentPosition = await L.marker([latitude, longitude], { icon: userIcon }).addTo(map).on('click', mapFly);
+        currentPosition.bindPopup("<b>Você está aqui</b>",customUserPopup).openPopup();
 
-        this.loginService.markers().subscribe((data) => {
+        // await map.panTo(currentPosition.getLatLng())
+
+        await this.loginService.markers().subscribe((data) => {
           data.marker.forEach(marker => {
 
-            const nws = L.marker([marker.latitude, marker.longitude], { icon: nwsIcon }).addTo(map).on('click',mapFly)
+            const nws = L.marker([marker.latitude, marker.longitude], { icon: nwsIcon }).addTo(map).on('click', mapFly)
             nws.bindPopup(`<b>${marker.name}</b>`, customNwsPopup);
           })
         })
 
-       function mapFly(event: any){
+        function mapFly(event: any) {
 
-          map.flyTo(event.latlng,17);
-          
+          map.flyTo(event.latlng, 17, {
+            animate: true,
+            duration: .5
+          });
+
         }
-        const currentPosition = L.marker([latitude, longitude], { icon: myIcon }).addTo(map).on('click',mapFly)   ;
-        currentPosition.bindPopup("Você está aqui").openPopup();
 
-      }, err => console.error(err), this.option);
+      }, err => console.error(err), this.config);
     }
+  }
+
+  center() {
+    const { latitude, longitude } = this.geoLocation.coords;
+    this.map.flyTo([latitude, longitude], 15, {
+      animate: true,
+      duration: .5
+    });
   }
 
 
